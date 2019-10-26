fa240136ed0aeb9dad6147379b6ea56f96c44cbf
Kasey Chuang <chuangky@uci.edu>
2019-10-26 08:50:47 -0700

    started sleep backend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
+totalPoints = 0
+
 
 @app.route("/")
 def redir():
@@ -8,12 +10,26 @@ def redir():
 
 @app.route("/categories")
 def goToCategoriesPage():
-    return render_template('categories.html')
+    return render_template('categories.html', totalPoints = totalPoints)
 
 
 @app.route("/fitness")
 def goToFitnessPage():
     return render_template('fitness.html')
+
+@app.route("/sleep")
+def goToSleepPage():
+    return render_template('sleep.html')
+
+@app.route("/inputSleep", methods = ['POST', 'GET'])
+def enterHours():
+    global totalPoints
+    if request.method == 'POST':
+        hours = request.form['hours']
+        points = hours * 10
+        totalPoints += points
+
+        return redirect("/categories")
 
 
 
