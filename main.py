ab1e8c46249ece88300a2e1561c04efeba3bcdd9
Rebecca Huang <rebeccaxhuang@gmail.com>
2019-10-26 02:43:01 -0700

    edit main.py and index.html

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def redir():
     return render_template('index.html')
 
+@app.route("/fitness")
+def goToFitnessPage():
+    return render_template('fitness.html')
+    
+
 
 if __name__ == "__main__":
     app.run()
