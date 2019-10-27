4a091ad8aee8ad7ab6afa2572509d50f6a8bed57
Rebecca Huang <rebeccaxhuang@gmail.com>
2019-10-26 17:07:55 -0700

    implemented nutritionix api to find calorie intake

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 totalPoints = 0
 username = ""
+calories = 2000
 
 from nutritionix import Nutritionix
 nix = Nutritionix(app_id="cf89e5fd", api_key="a5dbbc411a9326eb6c9391e93071677e")
@@ -16,11 +17,11 @@ def redir():
 
 @app.route("/categories")
 def goToCategoriesPage():
-    return render_template('categories.html', totalPoints = totalPoints)
+    return render_template('categories.html', totalPoints = totalPoints, calories = calories)
 
 @app.route("/diet")
 def goToDietPage():
-    return render_template('diet.html', totalPoints = totalPoints)
+    return render_template('diet.html', totalPoints = totalPoints, calories = calories)
 
 @app.route("/fitness")
 def goToFitnessPage():
@@ -43,13 +44,28 @@ def enterHours():
 
 @app.route("/inputFood", methods = ['POST', 'GET'])
 def enterFood():
+    global totalPoints
+    global calories
+
     if request.method == 'POST':
         foodInput = request.form['foodItem']
-        nix.search(foodInput)
-        results = foodInput.json()
-        print(results)
 
-        print(nix.search().nxql(fields=["nf_calories"]).json())
+        if foodInput.split()[0] == "ADD":
+            calories += int(foodInput.split()[1])
+
+        else:
+            food = nix.search(foodInput, results="0:1")
+            results = food.json()
+
+            foodID = results[u'hits'][0][u'_id']
+            item = nix.item(id=foodID).json()
+            foodCalories = item[u'nf_calories']
+
+            calories -= foodCalories
+            if calories < 0:
+                totalPoints -= 100
+                
+        return redirect("/diet")
 
 
 
